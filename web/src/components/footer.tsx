8f6ea391e97a760cab829dd2fb66fefa5180d5ec
im <83586607+exodie@users.noreply.github.com>
2024-01-17 12:41:47 +0400

    refactor prettier footer layout

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -1,5 +1,9 @@
 export const Footer = () => {
-    return <footer className="w-full p-2 flex mb-0 mt-auto">
-        <span className="text-white font-medium text-[10px]">*Работает только с ПрИ-23</span>
+  return (
+    <footer className="w-full p-2 flex mb-0 mt-auto">
+      <span className="text-white font-medium text-[10px]">
+        *Работает только с ПрИ-23
+      </span>
     </footer>
-}
+  );
+};
