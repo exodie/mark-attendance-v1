1fd3d356b1f3e04b7a699d893db438f5afbc73a9
im <83586607+exodie@users.noreply.github.com>
2024-01-18 02:48:01 +0400

    add new group-cart mocks value & create input component

--- web/src/__mocks__/group-cart.mocks.ts
+++ web/src/__mocks__/group-cart.mocks.ts
@@ -15,4 +15,256 @@ export const __group_cart__: Group[] = [
       },
     ],
   },
+  {
+    title: "2 курс - РКБС-24",
+    description: "Факультет крутости и безбашенности",
+    responsiblePerson: [
+      {
+        name: "Test Testov",
+        link: "https://google.com",
+      },
+      {
+        name: "Admin Adminov",
+        link: "https://google.com",
+      },
+    ],
+  },
+  {
+    title: "2 курс - РКБС-25",
+    description: "Факультет крутости и безбашенности",
+    responsiblePerson: [
+      {
+        name: "Test Testov",
+        link: "https://google.com",
+      },
+      {
+        name: "Admin Adminov",
+        link: "https://google.com",
+      },
+    ],
+  },
+  {
+    title: "2 курс - РКБС-26",
+    description: "Факультет крутости и безбашенности",
+    responsiblePerson: [
+      {
+        name: "Test Testov",
+        link: "https://google.com",
+      },
+      {
+        name: "Admin Adminov",
+        link: "https://google.com",
+      },
+    ],
+  },
+  {
+    title: "2 курс - РКБС-27",
+    description: "Факультет крутости и безбашенности",
+    responsiblePerson: [
+      {
+        name: "Test Testov",
+        link: "https://google.com",
+      },
+      {
+        name: "Admin Adminov",
+        link: "https://google.com",
+      },
+    ],
+  },
+  {
+    title: "2 курс - РКБС-28",
+    description: "Факультет крутости и безбашенности",
+    responsiblePerson: [
+      {
+        name: "Test Testov",
+        link: "https://google.com",
+      },
+      {
+        name: "Admin Adminov",
+        link: "https://google.com",
+      },
+    ],
+  },
+  {
+    title: "2 курс - РКБС-29",
+    description: "Факультет крутости и безбашенности",
+    responsiblePerson: [
+      {
+        name: "Test Testov",
+        link: "https://google.com",
+      },
+      {
+        name: "Admin Adminov",
+        link: "https://google.com",
+      },
+    ],
+  },
+  {
+    title: "2 курс - РКБС-30",
+    description: "Факультет крутости и безбашенности",
+    responsiblePerson: [
+      {
+        name: "Test Testov",
+        link: "https://google.com",
+      },
+      {
+        name: "Admin Adminov",
+        link: "https://google.com",
+      },
+    ],
+  },
+  {
+    title: "2 курс - РКБС-31",
+    description: "Факультет крутости и безбашенности",
+    responsiblePerson: [
+      {
+        name: "Test Testov",
+        link: "https://google.com",
+      },
+      {
+        name: "Admin Adminov",
+        link: "https://google.com",
+      },
+    ],
+  },
+  {
+    title: "2 курс - РКБС-32",
+    description: "Факультет крутости и безбашенности",
+    responsiblePerson: [
+      {
+        name: "Test Testov",
+        link: "https://google.com",
+      },
+      {
+        name: "Admin Adminov",
+        link: "https://google.com",
+      },
+    ],
+  },
+  {
+    title: "2 курс - РКБС-22",
+    description: "Факультет крутости и безбашенности",
+    responsiblePerson: [
+      {
+        name: "Test Testov",
+        link: "https://google.com",
+      },
+      {
+        name: "Admin Adminov",
+        link: "https://google.com",
+      },
+    ],
+  },
+  {
+    title: "3 курс - РКБС-23",
+    description: "Факультет крутости и безбашенности",
+    responsiblePerson: [
+      {
+        name: "Test Testov",
+        link: "https://google.com",
+      },
+      {
+        name: "Admin Adminov",
+        link: "https://google.com",
+      },
+    ],
+  },
+  {
+    title: "1 курс - РКБС-23",
+    description: "Факультет крутости и безбашенности",
+    responsiblePerson: [
+      {
+        name: "Test Testov",
+        link: "https://google.com",
+      },
+      {
+        name: "Admin Adminov",
+        link: "https://google.com",
+      },
+    ],
+  },
+  {
+    title: "1 курс - РКБС-23",
+    description: "Факультет крутости и безбашенности",
+    responsiblePerson: [
+      {
+        name: "Test Testov",
+        link: "https://google.com",
+      },
+      {
+        name: "Admin Adminov",
+        link: "https://google.com",
+      },
+    ],
+  },
+  {
+    title: "1 курс - РКБС-23",
+    description: "Факультет крутости и безбашенности",
+    responsiblePerson: [
+      {
+        name: "Test Testov",
+        link: "https://google.com",
+      },
+      {
+        name: "Admin Adminov",
+        link: "https://google.com",
+      },
+    ],
+  },
+  {
+    title: "4 курс - РКБС-23",
+    description: "Факультет крутости и безбашенности",
+    responsiblePerson: [
+      {
+        name: "Test Testov",
+        link: "https://google.com",
+      },
+      {
+        name: "Admin Adminov",
+        link: "https://google.com",
+      },
+    ],
+  },
+  {
+    title: "4 курс - РКБС-23",
+    description: "Факультет крутости и безбашенности",
+    responsiblePerson: [
+      {
+        name: "Test Testov",
+        link: "https://google.com",
+      },
+      {
+        name: "Admin Adminov",
+        link: "https://google.com",
+      },
+    ],
+  },
+  {
+    title: "4 курс - РКБС-23",
+    description: "Факультет крутости и безбашенности",
+    responsiblePerson: [
+      {
+        name: "Test Testov",
+        link: "https://google.com",
+      },
+      {
+        name: "Admin Adminov",
+        link: "https://google.com",
+      },
+    ],
+  },
+  {
+    title: "3 курс - РКБС-23",
+    description: "Факультет крутости и безбашенности",
+    responsiblePerson: [
+      {
+        name: "Test Testov",
+        link: "https://google.com",
+      },
+      {
+        name: "Admin Adminov",
+        link: "https://google.com",
+      },
+    ],
+  },
 ];
