f08a87e64e6c93bef0beebc3da8f659f9486cc8f
im <83586607+exodie@users.noreply.github.com>
2024-01-17 18:07:38 +0400

    create group-cart mocks

---
+++ web/src/__mocks__/group-cart.mocks.ts
@@ -0,0 +1,18 @@
+import { Group } from "@/interfaces";
+
+export const __group_cart__: Group[] = [
+  {
+    title: "2 курс - РКБС-23",
+    description: "Факультет крутости и безбашенности",
+    responsiblePerson: [
+      {
+        name: "Test Testov",
+        link: "https://google.com",
+      },
+      {
+        name: "Admin Adminov",
+        link: "https://google.com",
+      },
+    ],
+  },
+];
