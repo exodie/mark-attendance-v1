951c2c6d1bc1793f91016f1457a04f5283845a5b
mitrofanov <83586607+exodie@users.noreply.github.com>
2024-01-17 19:01:44 +0400

    Update telegram.sh

--- telegram.sh
+++ telegram.sh
@@ -6,8 +6,4 @@ REPOSITORY_NAME=$(basename -s .git `git config --get remote.origin.url`)
 TELEGRAM_MESSAGE="Новый коммит в репозитории $REPOSITORY_NAME!
 Сообщение коммита: $COMMIT_MESSAGE"
 
-curl -s -X POST \
-   https://api.telegram.org/bot6710558601:AAEmZ5WhpQyTJ2DMUnJD-GsQ4MiAVq_4RIE/sendMessage \ 
-   -d chat_id=-1001926355641 \ 
-   -d text="$TELEGRAM_MESSAGE" \ 
-   -d message_thread_id=10
+curl -s -X POST https://api.telegram.org/bot6710558601:AAEmZ5WhpQyTJ2DMUnJD-GsQ4MiAVq_4RIE/sendMessage?chat_id=-1001926355641&text=$TELEGRAM_MESSAGE&message_thread_id=10
