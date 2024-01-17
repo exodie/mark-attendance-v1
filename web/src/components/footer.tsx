860c397ad7577bd31408b7d5a052cbf0612e78c2
im <83586607+exodie@users.noreply.github.com>
2024-01-17 12:16:29 +0400

    add footer as components & refactor main layout

---
+++ web/src/components/footer.tsx
@@ -0,0 +1,5 @@
+export const Footer = () => {
+    return <footer className="w-full p-2 flex mb-0 mt-auto">
+        <span className="text-white font-medium text-[10px]">*Работает только с ПрИ-23</span>
+    </footer>
+}
