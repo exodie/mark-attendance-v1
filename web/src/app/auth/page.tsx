3d9e6d358aaf736cf8dec7fb96b71672c5a56487
im <83586607+exodie@users.noreply.github.com>
2024-01-18 02:48:41 +0400

    refactor auth-page layouts

--- web/src/app/auth/page.tsx
+++ web/src/app/auth/page.tsx
@@ -1,20 +1,34 @@
 import { GroupCart } from "@/components/cart/group-cart";
 
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 import { __group_cart__ } from "@/__mocks__/group-cart.mocks";
 
 export default function AuthGroupSelection() {
   return (
-    <div>
-      {__group_cart__.map(
-        ({ title, description, responsiblePerson }, index) => (
-          <GroupCart
-            key={index}
-            title={title}
-            description={description}
-            responsiblePerson={responsiblePerson}
-          />
-        )
-      )}
-    </div>
+    <section className="flex flex-col m-auto">
+      <nav className="flex flex-row items-center w-full p-10">
+        <h1 className="font-medium text-2xl">Выберите вашу группу</h1>
+
+        <div className="flex flex-row items-center gap-2 max-w-sm mr-0 ml-auto">
+          <Input placeholder="Поиск" type="text" />
+          <Button type="submit">Искать</Button>
+        </div>
+      </nav>
+
+      <div className="px-10 flex flex-row flex-wrap gap-4">
+        {__group_cart__.map(
+          ({ title, description, responsiblePerson }, index) => (
+            <GroupCart
+              key={index}
+              title={title}
+              description={description}
+              responsiblePerson={responsiblePerson}
+            />
+          )
+        )}
+      </div>
+    </section>
   );
 }
