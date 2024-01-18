8763d23204a749ebba76b6cc586949e115a0f43e
im <83586607+exodie@users.noreply.github.com>
2024-01-18 15:55:27 +0400

    refactor [0.1.4] auth-page layout

--- web/src/app/auth/page.tsx
+++ web/src/app/auth/page.tsx
@@ -1,20 +1,14 @@
-import { GroupCart } from "@/components/cart/group-cart";
-
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import { GroupCart, SearchComponent } from "@/components";
 
 import { __group_cart__ } from "@/__mocks__/group-cart.mocks";
 
 export default function AuthGroupSelection() {
   return (
     <section className="flex flex-col m-auto">
-      <nav className="flex flex-row items-center w-full p-10">
-        <h1 className="font-medium text-2xl">Выберите вашу группу</h1>
+      <nav className="flex sm:flex-row md:flex-row lg:flex-row flex-col items-center justify-center w-full p-10">
+        <h1 className="font-medium text-xl">Выберите вашу группу</h1>
 
-        <div className="flex flex-row items-center gap-2 max-w-sm mr-0 ml-auto">
-          <Input placeholder="Поиск" type="text" />
-          <Button type="submit">Искать</Button>
-        </div>
+        <SearchComponent />
       </nav>
 
       <div className="px-10 flex flex-row flex-wrap gap-4">
