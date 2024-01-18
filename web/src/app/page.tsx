2d99197f62df0482b542fe4fbb2992582e4919f9
im <83586607+exodie@users.noreply.github.com>
2024-01-18 15:59:59 +0400

    dev-test: refactor app-page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,9 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-between p-24">home</section>
+    <section className="flex flex-col items-center justify-between p-24">
+      <Link href={"/auth"}>Тест: Выбор группы</Link>
+    </section>
   );
 }
