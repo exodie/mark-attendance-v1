3d33b7103c4ecf10dbc8822f2d2617757b45bcaa
im <83586607+exodie@users.noreply.github.com>
2024-01-29 16:38:12 +0400

    refactor navigation link to groups

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-between p-24">
-      <Link href={"/auth"}>Тест: Выбор группы</Link>
+      <Link href={"/groups"}>Тест: Выбор группы</Link>
     </section>
   );
 }
