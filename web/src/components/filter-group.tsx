963b2302fe1f8409b9e486dad5782febdadc6a28
im <83586607+exodie@users.noreply.github.com>
2024-01-29 16:01:17 +0400

    add [beta] filter groups
    - No have auto updating with search component [BUG]

---
+++ web/src/components/filter-group.tsx
@@ -0,0 +1,27 @@
+import { Group } from "@/interfaces";
+import { FC } from "react";
+import { GroupCart } from ".";
+
+interface Props {
+  groups: Group[];
+  searchParams: string;
+}
+
+export const FilteredGroupComponent: FC<Props> = (props) => {
+  const filteredGroups = props.groups.filter((group) =>
+    group.title.toLowerCase().includes(props.searchParams.toLowerCase())
+  );
+
+  return (
+    <div className="flex flex-row flex-wrap gap-4">
+      {filteredGroups.map((item, index) => (
+        <GroupCart
+          key={index}
+          title={item.title}
+          description={item.description}
+          responsiblePerson={item.responsiblePerson}
+        />
+      ))}
+    </div>
+  );
+};
