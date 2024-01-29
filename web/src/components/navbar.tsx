106bf15fdde44e3af36379b31f99025300e055ba
im <83586607+exodie@users.noreply.github.com>
2024-01-29 15:56:57 +0400

    refactor navbar component
    - Ссылки на "/" страницу и поддержку меняют цвет при наведении (возможен баг в мобильной версии)
    - Исправлен баг при обновлении страницы появлялось консольное предупреждение

--- web/src/components/navbar.tsx
+++ web/src/components/navbar.tsx
@@ -11,22 +11,26 @@ import {
   ScrollArea,
 } from "./ui";
 
-import { version } from "../../package.json";
+import packageInfo from "../../package.json";
 
 export const Navbar = () => {
   return (
     <nav className="flex flex-row justify-between items-center m-auto w-full h-16 px-10 py-4">
-      <h1 className="font-bold text-2xl">
+      <h1 className="font-bold text-2xl hover:text-primary/90">
         <Link href={"/"}>ISTracker</Link>
       </h1>
 
       <div>
         <Sheet>
-          <SheetTrigger>Поддержка</SheetTrigger>
+          <SheetTrigger>
+            <span className="hover:text-primary/90">Поддержка</span>
+          </SheetTrigger>
           <SheetContent>
             <SheetHeader>
               <SheetTitle>Все необходимое здесь</SheetTitle>
-              <SheetDescription>Текущая версия: {version}</SheetDescription>
+              <SheetDescription>
+                Текущая версия: {packageInfo.version}
+              </SheetDescription>
             </SheetHeader>
 
             <ScrollArea className="h-full rounded-md border p-4">
