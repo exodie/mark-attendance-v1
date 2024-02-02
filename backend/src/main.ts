83db7ba9b460456bfa0d3b42b204c73fa2729da8
im <83586607+exodie@users.noreply.github.com>
2024-02-02 12:25:12 +0400

    add [0.1.1] first swagger config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { config } from 'dotenv';
+
 import { AppModule } from './app.module';
+
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('ISTracker')
+    .setDescription('The tracker API description')
+    .setVersion('0.1.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
