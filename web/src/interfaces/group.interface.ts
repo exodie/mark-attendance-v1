09d0d4be02f570477125a34e62d40144eb60bc8e
im <83586607+exodie@users.noreply.github.com>
2024-02-02 12:39:04 +0400

    add JSDoc to interface Group

--- web/src/interfaces/group.interface.ts
+++ web/src/interfaces/group.interface.ts
@@ -1,4 +1,11 @@
+/**
+ * @param id generated from backend
+ * @param title name group
+ * @param description faculty naming or ...
+ * @param responsiblePerson links to the "main ones" in the group
+ */
 export interface Group {
+  id: string;
   title: string;
   description: string;
   responsiblePerson: {
