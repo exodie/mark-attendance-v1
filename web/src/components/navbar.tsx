be3ded977b157fe95860523cd4fc8e3faf5527b2
im <83586607+exodie@users.noreply.github.com>
2024-02-02 12:25:22 +0400

    refactor layout navbar components

--- web/src/components/navbar.tsx
+++ web/src/components/navbar.tsx
@@ -15,7 +15,7 @@ import packageInfo from "../../package.json";
 
 export const Navbar = () => {
   return (
-    <nav className="flex flex-row justify-between items-center m-auto w-full h-16 px-10 py-4">
+    <nav className="w-full flex flex-row justify-between items-center m-auto h-16 px-10 py-4">
       <h1 className="font-bold text-2xl hover:text-primary/90">
         <Link href={"/"}>ISTracker</Link>
       </h1>
