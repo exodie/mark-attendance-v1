d1e815486e418912f8f8e8f7875c1cc599818571
im <83586607+exodie@users.noreply.github.com>
2024-02-02 12:52:22 +0400

    add layout for authorization to account

--- web/src/app/auth/signin/[slug]/page.tsx
+++ web/src/app/auth/signin/[slug]/page.tsx
@@ -1,3 +1,32 @@
-export default function SignInWithSlugs ({ params }: { params: { slug: string } }) {
-    return <div className="text-white">signin::{params.slug}</div>
+import { Button, Input } from "@/components/ui";
+import Link from "next/link";
+
+export default function SignInWithSlugs() {
+  return (
+    <form className="w-8/12 lg:w-4/12 flex flex-col mt-80 gap-2">
+      <h1 className="mx-auto text-xl">Авторизация</h1>
+      <Input
+        minLength={4}
+        maxLength={16}
+        type="text"
+        placeholder="Логин"
+        className="w-full text-lg"
+      />
+      <Input
+        minLength={8}
+        maxLength={32}
+        type="password"
+        placeholder="Пароль"
+        className="w-full text-lg"
+      />
+      <Button className="w-full text-lg">Продолжить</Button>
+      <hr />
+      <div className="flex flex-row items-center ml-0 gap-2">
+        <label>Хотите добавить группу?</label>
+        <Link href={"/addGroup"}>
+          <Button size={"default"}>Добавить</Button>
+        </Link>
+      </div>
+    </form>
+  );
 }
