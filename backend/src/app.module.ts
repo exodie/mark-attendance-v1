31dedd361c907a6fb6c6c61d2f0efac825879e2e
im <83586607+exodie@users.noreply.github.com>
2024-02-06 21:23:55 +0400

    delete app controller & service
    - Refactor app.module
    - Add Mongoose connect to database uri from .env (check .env.example)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { config } from 'dotenv';
+
+config();
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(process.env.DATABASE_URI),
+  ],
 })
 export class AppModule {}
