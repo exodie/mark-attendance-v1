8e8ea690caba3b55d5701fa9b37cd4eaee3c4c41
im <83586607+exodie@users.noreply.github.com>
2024-02-06 21:37:31 +0400

    refactor [0.1.3] app.module & pckg-version

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { config } from 'dotenv';
+import { UsersModule } from './entities/users/users.module';
+import { GroupsModule } from './entities/groups/groups.module';
 
 config();
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.DATABASE_URI),
+    UsersModule,
+    GroupsModule,
   ],
 })
 export class AppModule {}
